d3c3b53a0dcd2bb9908259b617b009d1523c5068
charmseer <charmseer@gmail.com>
2021-05-31 00:15:06 +0530

    Add Scripts for Camera Follow

---
+++ platformer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+
+    [SerializeField] private Vector3 offset;
+
+    [SerializeField] [Range(0.01f,1f)]
+    private float smoothSpeed = 0.125f;
+
+    private Vector3 velocity = Vector3.zero;
+
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+    }
+}
