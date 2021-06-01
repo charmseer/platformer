f34ba4df01acef949bd4f6967f7a561e74b25822
charmseer <charmseer@gmail.com>
2021-06-01 17:53:49 +0530

    Chest Animation and Level Restart on Completion

--- platformer/Assets/Scripts/EnemyMace.cs
+++ platformer/Assets/Scripts/EnemyMace.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMace : MonoBehaviour
-{   
+{
+    [SerializeField] private float amplitude = 2;
     private Vector3 startPos;
 
     // Start is called before the first frame update
@@ -15,6 +16,6 @@ public class EnemyMace : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = startPos + new Vector3(0f, Mathf.Sin(Time.time),0f);
+        transform.position = startPos + amplitude * new Vector3(0f, Mathf.Sin(Time.time),0f);
     }
 }
---
+++ platformer/Assets/Scripts/Win.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<Animator>().SetTrigger("Open");
+        }
+    }
+}
