09a9838e5f01d2fe4445eb5c14c7b47f2fbc1199
charmseer <charmseer@gmail.com>
2021-06-01 14:05:58 +0530

    Add Up and Down Motion to Enemy Mace

---
+++ platformer/Assets/Scripts/EnemyMace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMace : MonoBehaviour
+{   
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPos + new Vector3(0f, Mathf.Sin(Time.time),0f);
+    }
+}
